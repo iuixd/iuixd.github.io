ac59dad4b5e49b9fcb00b40f9fdc42df23e80cb6
Srikumar K <text2sri@gmail.com>
2025-07-01 17:41:11 +0530

    Updated navigation components

--- src/components/GitHub.jsx
+++ src/components/GitHub.jsx
@@ -16,9 +16,9 @@ const GitHub = () => {
       <div className="flex flex-col items-center relative transition-all duration-200 ease-in-out">
         
         <div className="text-white flex flex-col self-auto w-[1024px] h-auto static">
-                    <Link to="/" className="mx-24 mt-[12px] self-start relative z-101">
-                <img src={myPhoto} className="w-12 h-12" alt="Srikumar's Photo" x-transition="true"/>
-            </Link>
+          <Link to="/" className="mx-24 mt-[12px] self-start relative z-101">
+            <img src={myPhoto} className="w-12 h-12" alt="Srikumar's Photo" x-transition="true"/>
+          </Link>
         </div>
       </div>
     </>
