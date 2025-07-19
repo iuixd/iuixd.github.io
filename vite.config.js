b34a0fc3fd019cf7374a4f033ef20f8c5c22bd35
Srikumar <srikumar.design@gmail.com>
2025-07-19 16:05:16 +0530

    installed vite-sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://www.srikumar.design/',
+    }),
+  ],
 })
