f7f252bbd22a657ae53c9eb7702564dafbe55a55
anujsrijan <anuj.srijan@gmail.com>
2025-08-24 12:49:36 +0530

    updated contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -52,7 +52,7 @@ const Contact = () => {
             <section className="flex flex-col text-turquoise-900 font-medium w-full h-fit px-[20%] max-[540px]:px-[12%] max-[540px]:mt-[30px]">
               <div className="flex flex-col justify-center items-center">
                 <Reveal>
-                  <h1 className="flex mb-16 justify-center font-extrabold text-7xl max-[540px]:text-5xl text-white">
+                  <h1 className="flex w-fit h-[122px] ml-[160px] mb-16 pt-[90px] pl-[34px] justify-center font-extrabold text-7xl max-[540px]:text-5xl text-white bg-[url('../assets/bauhas-shape.webp')] bg-no-repeat bg-left min-[200px]:text-shadow-md min-md:text-shadow-lg">
                     Contact
                   </h1>
                 </Reveal>
